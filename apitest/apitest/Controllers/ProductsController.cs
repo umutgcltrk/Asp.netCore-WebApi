a0aede6b70656d41973bf08337a2620e6cb01adf
umutgcltrk <95372849+umutgcltrk@users.noreply.github.com>
2022-09-23 18:28:04 +0300

    Add files via upload

---
+++ apitest/apitest/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using apitest.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace apitest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly Context _context;
+        public ProductsController(Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult getProducts()
+        {
+            return Ok(_context.Products.ToList());
+        }
+        [HttpPut("{id}")]
+        public IActionResult updateProduct(int id,Product product)
+        {
+            var updated = _context.Products.FirstOrDefault(x => x.Id == id);
+            updated.Name = product.Name;
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult deleteProduct(int id)
+        {
+            var deleteProduct = _context.Products.FirstOrDefault(x => x.Id == id);
+            _context.Remove(deleteProduct);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpPost]
+        public IActionResult addProducts(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+            return Created("", product);
+        }
+    }
+}
---
+++ apitest/apitest/Entities/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace apitest.Entities
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("")
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
